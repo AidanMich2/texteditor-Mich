c14b18ccdb1110726a7a0d068ea84013e4ae4220
AidanMich <143116012+AidanMich@users.noreply.github.com>
2025-10-05 21:39:02 -0500

    finished draft of GapBuffer

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -4,35 +4,82 @@ package edu.grinnell.csc207.texteditor;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
+    private int size;
+    private int sizeOfArray;
+    private char [] arr;
+    private int firstIndexGap;
+    private int firstAfterGap;
+    private int position;
+
+    public GapBuffer (){
+        this.size = 0;
+        this.sizeOfArray = 100;
+        arr = new char [sizeOfArray];
+        this.position = 0;
+    }
     public void insert(char ch) {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        if (firstAfterGap - firstIndexGap > 0){
+            arr[firstIndexGap] = ch;
+            size++;
+        }
+        else{
+            expand ();
+            sizeOfArray = sizeOfArray *2;
+            arr[firstIndexGap] = ch;
+            size++;
+        }
+    }
+
+    public void expand (){
+        char [] newArray = new char [sizeOfArray * 2];
+        for (int i = 0; i <= firstIndexGap; i++){
+            newArray [i] = arr[i];
+        }
+        firstAfterGap = sizeOfArray + firstIndexGap;
+        for (int i = firstAfterGap; i <= sizeOfArray; i++){
+            newArray [i] = arr[i];
+        }
+        arr = newArray;
     }
 
     public void delete() {
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        firstIndexGap--;
+        size--;
     }
 
     public int getCursorPosition() {
-        throw new UnsupportedOperationException("Unimplemented method 'getCursorPosition'");
+        return position;
     }
 
     public void moveLeft() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+        position--;
     }
 
     public void moveRight() {
-        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+        position++;
     }
 
     public int getSize() {
-        throw new UnsupportedOperationException("Unimplemented method 'getSize'");
+        return size;
     }
 
     public char getChar(int i) {
-        throw new UnsupportedOperationException("Unimplemented method 'getChar'");
+        if (i >= 0 && i < size){
+            return arr[i];
+        }
+        else{
+            throw new IndexOutOfBoundsException ();
+        }
     }
 
     public String toString() {
-        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+        String str = "";
+        for (int i = 0; i <firstIndexGap; i++){
+            str += arr[i];
+        }
+        for(int i = firstAfterGap; i< sizeOfArray; i++){
+            str += arr[i];
+        }
+        return str;
     }
 }
